fa27a7e090db55bc98bcc31a41cd9a914bfe3a7d
Arvin Villadelgado <Arvin Villadelgado>
2017-04-17 23:39:06 -0700

    don't know JAVA but i tried

--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -18,38 +18,67 @@ import java.util.Collection;
 
 final class MyJSON implements JSON {
 
+
+  public HashMap<String, JSON> map;
+  map.set("name", JSON.parse(value))
+  //constructor
   public MyJSON() {
-    dict = new Dictionary();
+    map = new HashMap();
   }
+
+  // Get the value of the nested object with the given name. If there is
+  // no nested object with that name, the method will return null.
   @Override
   public JSON getObject(String name) {
-
-    return null;
+    if (map.containsKey(name) {
+      firstValue = map.get(name); //first value is another object
+      value = firstValue.get(name)
+      return value;
+    else {
+      return null;
+      }
+    }
   }
 
+  // Set the value of the nested object with the given name. Any old value
+  // should be overwritten. This method will always return a reference to
+  // "this".
   @Override
   public JSON setObject(String name, JSON value) {
-    dict.put(name, value);
-    return value;
-  }
-
-  @Override
-  public String getString(String name) {
-    // TODO: implement this
-    return null;
-  }
-
-  @Override
-  public JSON setString(String name, String value) {
-    // TODO: implement this
+    map.put(name, value);
     return this;
   }
 
+
+  // Get the string value within this object that has the given name. if
+  // there is no string with the given name, the method will return null.
+  @Override
+  public String getString(String name) {
+    if (map.containsKey(name) {
+      value = map.get(name);
+      return value;
+    else {
+      return null;
+      }
+    }
+  }
+
+  // Set the string that should be stored under the given name. Any old value
+  // should be overwritten. This method will always return a reference to
+  // "this".
+  @Override
+  public JSON setString(String name, String value) {
+    map.get(name) = value
+    return this;
+  }
+
+  // Copy the names of all object values to the given collection.
   @Override
   public void getObjects(Collection<String> names) {
     // TODO: implement this
   }
 
+  // Copy the names of all string values to the given collection.
   @Override
   public void getStrings(Collection<String> names) {
     // TODO: implement this
